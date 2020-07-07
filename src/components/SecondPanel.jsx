ab6c8a8e69412e08bde3bcb54c40976e78b23533
michal <zendran.m@gmail.com>
2020-07-07 16:20:07 +0200

    Svg background changes

--- src/components/SecondPanel.jsx
+++ src/components/SecondPanel.jsx
@@ -1,51 +1,109 @@
 import React, { useState, useEffect } from "react";
 import "styles/SecondPanel.scss";
+import SectionTitle from "components/SectionTitle";
 
 const SecondPanel = () => {
-  const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 900);
+  const [polygonHeight, setPolygonHeight] = useState(800);
+  const [polygonAngle, setPolygonAngle] = useState(isDesktop ? 150 : 100);
+  const [polygonPoints, setPolygonPoints] = useState(
+    "0," +
+      polygonAngle +
+      " " +
+      window.innerWidth +
+      ",0 " +
+      window.innerWidth +
+      "," +
+      polygonHeight +
+      " " +
+      window.innerWidth / 2 +
+      "," +
+      (polygonHeight + 70) +
+      " 0," +
+      polygonHeight
+  );
 
   const updateBackground = () => {
-    setDesktop(window.innerWidth > 900);
+    setIsDesktop(window.innerWidth > 900);
   };
 
+  const updatePolygonHeight = () => {
+    let targetHeight;
+    if (window.innerWidth < 900) {
+      targetHeight = 1500;
+    } else {
+      targetHeight = 500;
+    }
+    setPolygonHeight(targetHeight);
+    console.log(targetHeight);
+  };
+
+  const updatePolygonAngle = () => {
+    setPolygonAngle(isDesktop ? 150 : 100);
+  };
+
+  const updatePolygonPoints = () => {
+    setPolygonPoints(
+      "0," +
+        polygonAngle +
+        " " +
+        window.innerWidth +
+        ",0 " +
+        window.innerWidth +
+        "," +
+        polygonHeight +
+        " " +
+        window.innerWidth / 2 +
+        "," +
+        (polygonHeight + 70) +
+        " 0," +
+        polygonHeight
+    );
+    console.log(polygonHeight);
+  };
+
+  useEffect(() => {
+    updateBackground();
+    updatePolygonHeight();
+    updatePolygonAngle();
+    updatePolygonPoints();
+    console.log("I run");
+  }, []);
   useEffect(() => {
     window.addEventListener("resize", updateBackground);
     return () => window.removeEventListener("resize", updateBackground);
   });
 
-  const polygonHeight = 200;
-  let polygonAngle = isDesktop ? 20 : 40;
-  const polygonPoints =
-    "0," +
-    polygonAngle +
-    " 200,0 200," +
-    polygonHeight +
-    " 100," +
-    (polygonHeight + 30) +
-    " 0," +
-    polygonHeight;
+  useEffect(() => {
+    window.addEventListener("resize", updatePolygonHeight);
+    return () => window.removeEventListener("resize", updatePolygonHeight);
+  });
+
+  useEffect(() => {
+    window.addEventListener("resize", updatePolygonAngle);
+    return () => window.removeEventListener("resize", updatePolygonAngle);
+  });
+
+  useEffect(() => {
+    window.addEventListener("resize", updatePolygonPoints);
+    return () => window.removeEventListener("resize", updatePolygonPoints);
+  });
 
   return (
     <div className="SecondPanel">
       <div className="mySVG">
-        <svg viewBox="0 0 200 230">
+        <svg width={window.innerWidth} height={polygonHeight + 70}>
           <polygon points={polygonPoints} />
         </svg>
       </div>
       <div className="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla blandit
-        lacus non felis tempus suscipit. Integer id justo vitae massa ultrices
-        posuere. Nam sem tellus, tincidunt id enim id, sollicitudin interdum
-        nibh. Quisque lobortis ultricies pharetra. Vivamus iaculis venenatis
-        risus quis dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Sed at mi lorem. Praesent ac accumsan neque. Proin cursus
-        vestibulum ligula, et malesuada leo rutrum vitae. Duis tristique nibh
-        vel felis maximus, eu hendrerit urna aliquet. Sed vel purus ac lacus
-        pulvinar porta. Aenean rutrum tincidunt augue id commodo. Etiam
-        imperdiet ipsum leo, ut egestas ex mollis non. Proin sit amet congue mi,
-        non sagittis arcu.
+        <SectionTitle text="PROJECTS" />
+        <div className="projectList">
+          <div className="projectPanel">AAAAAA</div>
+          <div className="projectPanel">BBBBBB</div>
+          <div className="projectPanel">CCCCCC</div>
+        </div>
       </div>
-      <div class="mySVG ghost" />
     </div>
   );
 };
---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "styles/SectionTitle.scss";
+
+const SectionTitle = (props) => {
+  return <div className="SectionTitle">{props.text}</div>;
+};
+
+export default SectionTitle;
