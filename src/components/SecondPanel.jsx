374e3c245f64181a1fd9641530f58d2f4ad79ab2
michal <zendran.m@gmail.com>
2020-07-14 13:06:11 +0200

    SecondPanel improvement

--- src/components/SecondPanel.jsx
+++ src/components/SecondPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "styles/SecondPanel.scss";
 import SectionTitle from "components/SectionTitle";
 import ProjectPanel from "components/ProjectPanel";
@@ -7,82 +7,19 @@ import msc from "graphics/msc.png";
 import portfolio_app from "graphics/portfolio_app.png";
 
 const SecondPanel = () => {
-  const [isDesktop, setIsDesktop] = useState(document.body.clientWidth > 900);
-  const [polygonAngle, setPolygonAngle] = useState(isDesktop ? 2 : 2);
-  const [polygonPoints, setPolygonPoints] = useState(
-    "0," + polygonAngle + " 100,0 100,100 50,110 0,100"
-  );
-
-  const updateBackground = () => {
-    setIsDesktop(window.innerWidth > 900);
-  };
-
-  const updatePolygonAngle = () => {
-    let angle;
-    if (window.innerWidth > 1300) {
-      angle = 20;
-    } else if (window.innerWidth > 900) {
-      angle = 12;
-    } else if (window.innerWidth > 400) {
-      angle = 9;
-    } else if (window.innerWidth > 300) {
-      angle = 7;
-    } else {
-      angle = 10;
-    }
-    setPolygonAngle(angle);
-  };
-
-  const updatePolygonPoints = () => {
-    setPolygonPoints("0," + polygonAngle + " 100,0 100,100 50,110 0,100");
-  };
-
-  useEffect(() => {
-    updateBackground();
-    updatePolygonAngle();
-    updatePolygonPoints();
-  });
-
-  useEffect(() => {
-    window.addEventListener("resize", updateBackground);
-    return () => window.removeEventListener("resize", updateBackground);
-  });
-
-  useEffect(() => {
-    window.addEventListener("resize", updatePolygonAngle);
-    return () => window.removeEventListener("resize", updatePolygonAngle);
-  });
-
-  useEffect(() => {
-    window.addEventListener("resize", updatePolygonPoints);
-    return () => window.removeEventListener("resize", updatePolygonPoints);
-  });
-
   return (
     <div className="SecondPanel">
-      <div className="mySVG">
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 100 110"
-          preserveAspectRatio="none"
-        >
-          <polygon points={polygonPoints} />
-        </svg>
-      </div>
-      <div className="content">
-        <SectionTitle text="PROJECTS" />
-        <div className="projectList">
-          <ProjectPanel
-            image={task_manager}
-            text="Task Manager - organize all Your tasks in a convenient way"
-          />
-          <ProjectPanel
-            image={msc}
-            text="Research on methods of changing objects in images using Deepfake technology"
-          />
-          <ProjectPanel image={portfolio_app} text="My portfolio" />
-        </div>
+      <SectionTitle text="PROJECTS" />
+      <div className="projectList">
+        <ProjectPanel
+          image={task_manager}
+          text="Task Manager - organize all Your tasks in a convenient way"
+        />
+        <ProjectPanel
+          image={msc}
+          text="Research on methods of changing objects in images using Deepfake technology"
+        />
+        <ProjectPanel image={portfolio_app} text="My portfolio" />
       </div>
     </div>
   );
