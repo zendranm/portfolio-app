914cb9f6979afe44ffcd44b248cc34ffb1d4bc63
michal <zendran.m@gmail.com>
2020-07-14 15:30:37 +0200

    ProjectPanel changes

--- src/components/ProjectPanel.jsx
+++ src/components/ProjectPanel.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "styles/ProjectPanel.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 
 const ProjectPanel = (props) => {
   return (
@@ -7,7 +10,13 @@ const ProjectPanel = (props) => {
       className="ProjectPanel"
       style={{ backgroundImage: `url(${props.image})` }}
     >
-      <div className="coverPanel">{props.text}</div>
+      <div className="coverPanel">
+        <div className="title">{props.text}</div>
+        <div className="buttons">
+          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faPlayCircle} />
+        </div>
+      </div>
     </div>
   );
 };
