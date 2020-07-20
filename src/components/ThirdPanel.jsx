f3eabd8d9cb2803ac8bd99a6a317669ccadf7bc3
michal <zendran.m@gmail.com>
2020-07-20 15:54:08 +0200

    Third panel improvements

--- src/components/ThirdPanel.jsx
+++ src/components/ThirdPanel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "styles/ThirdPanel.scss";
 import SectionTitle from "components/SectionTitle";
-import image from "graphics/image.jpg";
+import image from "graphics/image_cropped.jpg";
 
 const ThirdPanel = () => {
   return (
@@ -12,17 +12,17 @@ const ThirdPanel = () => {
           <img src={image} alt="logo" />
         </div>
         <div className="content">
-          I am a student of the last year of Master’s degree at Wrocław
-          University of Science and Technology. I study Computer Science in
-          English at the faculty of Electronics. In my master’s thesis I
-          analyzed the problem of deepfake generation using artificial neural
-          networks. During my studies I developed skills of teamwork, time
-          management, effective communication and flexibility. I am eager to get
-          new experience and learn new technologies. I had opportunity to take
-          part in student internship at well-known international company as a
-          front-end developer. Thanks to that I developed my programming skills,
-          learned how to work in Scrum methodology and gained experience in
-          working in large, international team.
+          I am a graduate of master studies at Wrocław University of Science and
+          Technology in the field of Computer Science in English at the faculty
+          of Electronics. In my master’s thesis I analyzed the problem of
+          deepfake generation using artificial neural networks. During my
+          studies I developed skills of teamwork, time management, effective
+          communication and flexibility. I am eager to get new experience and
+          learn new technologies. I had opportunity to take part in student
+          internship at well-known international company as a front-end
+          developer. Thanks to that I developed my programming skills, learned
+          how to work in Scrum methodology and gained experience in working in
+          large, international team.
         </div>
       </div>
     </div>
