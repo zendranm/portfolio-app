fc34719bf8b3c9246bda2c97fb454223744ea4c3
michal <zendran.m@gmail.com>
2020-09-06 20:00:35 +0200

    New branch for design init

--- src/components/FirstPanel.jsx
+++ src/components/FirstPanel.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "styles/FirstPanel.scss";
 import HelloText from "components/HelloText";
+import logo from "graphics/snowboard.png";
 
 const FirstPanel = () => {
   return (
@@ -8,6 +9,9 @@ const FirstPanel = () => {
       <HelloText />
       <div className="NamePart">I'M MICHAŁ ZENDRAN</div>
       <div className="JobPart">FRONTEND DEVELOPER</div>
+      <div className="mainImage">
+        <img src={logo} alt="" />
+      </div>
     </div>
   );
 };
