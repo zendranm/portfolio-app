9f2cc712b121a96186e2f75cd42988a4519a0bc8
michal <zendran.m@gmail.com>
2020-09-23 19:08:44 +0200

    Stack icons labels

--- src/components/FourthPanel.jsx
+++ src/components/FourthPanel.jsx
@@ -24,45 +24,59 @@ const FourthPanel = () => {
       <div className="stackList">
         <div className="icon">
           <img src={html5} alt="html5" />
+          <div className="iconName">HTML5</div>
         </div>
         <div className="icon">
           <img src={css3} alt="css3" />
+          <div className="iconName">CSS3</div>
         </div>
         <div className="icon">
           <img src={javascript} alt="javascript" />
+          <div className="iconName">JAVASCRIPT</div>
         </div>
         <div className="icon">
           <img src={typescript} alt="typescript" />
+          <div className="iconName">TYPESCRIPT</div>
         </div>
         <div className="icon">
           <img src={react} alt="react" />
+          <div className="iconName">REACT</div>
         </div>
         <div className="icon">
           <img src={redux} alt="redux" />
+          <div className="iconName">REDUX</div>
         </div>
         <div className="icon">
           <img src={router} alt="router" />
+          <div className="iconName">ROUTER</div>
         </div>
         <div className="icon">
           <img src={webpack} alt="webpack" />
+          <div className="iconName">WEBPACK</div>
         </div>
         <div className="icon">
           <img src={sass} alt="sass" />
+          <div className="iconName">SASS</div>
         </div>
         <div className="icon">
           <img src={bootstrap} alt="bootstrap" />
+          <div className="iconName">BOOTSTRAP</div>
         </div>
         <div className="icon">
           <img src={fontawesome} alt="fontawesome" />
+          <div className="iconName">FONTAWESOME</div>
         </div>
         <div className="icon">
           <img src={firebase} alt="firebase" />
+          <div className="iconName">FIREBASE</div>
         </div>
         <div className="icon">
           <img src={python} alt="python" />
+          <div className="iconName">PYTHON</div>
         </div>
         <div className="icon">
           <img src={github} alt="github" />
+          <div className="iconName">GITHUB</div>
         </div>
       </div>
       <div className="bottomV" />
