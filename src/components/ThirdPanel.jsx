cb061ff761f104cc56d3492e86e69671252fe15b
michal <zendran.m@gmail.com>
2020-10-24 21:26:39 +0200

    Small icons styles changes

--- src/components/ThirdPanel.jsx
+++ src/components/ThirdPanel.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import "../styles/ThirdPanel.scss";
-import SectionTitle from "../components/SectionTitle";
-import SubSectionTitle from "../components/SubSectionTitle";
-import ImageComposition from "../components/ImageComposition";
+import React from "react"
+import "../styles/ThirdPanel.scss"
+import SectionTitle from "../components/SectionTitle"
+import SubSectionTitle from "../components/SubSectionTitle"
+import ImageComposition from "../components/ImageComposition"
 
-const ThirdPanel = (props) => {
+const ThirdPanel = props => {
   return (
     <div className="ThirdPanel" ref={props.customRef}>
       <div className="upperV" />
@@ -15,44 +15,29 @@ const ThirdPanel = (props) => {
         </div>
         <div className="rightBox">
           <div className="mainDescription">
-            Hi! I'm Michał,{" "}
-            <u>
-              <b>React Developer</b>
-            </u>
-            . Fresh out of college, but experienced, I am looking for an
-            opportunity to further develop my skills in React and Frontend in
-            general. I had the opportunity to take part in a student internship{" "}
-            <u>
-              <b>at well-known international company</b>
-            </u>{" "}
-            as a front-end developer. During this time I managed to implement
-            and deploy{" "}
-            <u>
-              <b>crucial application in React.</b>
-            </u>
+            I'm Michał, React Developer. Fresh out of college, but experienced,
+            I am looking for an opportunity to further develop my skills in
+            React and Frontend in general. I had the opportunity to take part in
+            a student internship at a well-known international company as a
+            front-end developer. During this time I managed to implement and
+            deploy a crucial application in React. At my current position I work
+            with JS everyday, implementing, debugging and consulting clients’
+            custom solutions.
           </div>
           <div className="subDescription">
             <SubSectionTitle text="Front-end Developer" />
             <div>
               I like to code things from scratch and love to see an idea
-              becoming a live thing. Clean code and elegant design is{" "}
-              <u>
-                <b>my ninja way.</b>
-              </u>
+              becoming a live thing. Clean code and elegant design is my ninja
+              way.
             </div>
           </div>
           <div className="subDescription">
             <SubSectionTitle text="Background" />
             <div>
-              I obtained my engineer degree in{" "}
-              <u>
-                <b>Control Engineering and Robotics</b>
-              </u>{" "}
+              I obtained my engineer degree in Control Engineering and Robotics
               but discovered that programming brings me tons of satisfaction and
-              obtained my{" "}
-              <u>
-                <b>master degree in Computer Science.</b>
-              </u>
+              so obtained my master degree in Computer Science.
             </div>
           </div>
           <div className="subDescription">
@@ -66,7 +51,7 @@ const ThirdPanel = (props) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ThirdPanel;
+export default ThirdPanel
