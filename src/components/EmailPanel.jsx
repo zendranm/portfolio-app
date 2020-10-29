9e22d9c6779a5b30f427b560a45b579e7df6a7ba
michal <zendran.m@gmail.com>
2020-10-29 21:08:48 +0100

    Form required fix

--- src/components/EmailPanel.jsx
+++ src/components/EmailPanel.jsx
@@ -7,11 +7,28 @@ const EmailPanel = () => {
       <div className="inputFields">
         <form name="contact" action="POST" data-netlify="true">
           <input type="hidden" name="form-name" value="contact" />
-          <input type="text" id="name" name="name" placeholder="Name" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            required
+          />
           <br />
-          <input type="email" id="email" name="email" placeholder="Email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
           <br />
-          <textarea id="message" name="message" placeholder="Message" />
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Message"
+            required
+          />
           <br />
           <input type="submit" id="submit" name="submit" value="Submit" />
         </form>
